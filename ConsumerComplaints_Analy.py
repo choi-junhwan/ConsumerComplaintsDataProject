859c5f68aa6935bd141814c34fa3ef20128d09d1
Junhwan Choi <choi.junhwan@gmail.com>
2016-01-09 21:16:35 -0600

    Initial addition

---
+++ ConsumerComplaints_Analy.py
@@ -0,0 +1,216 @@
+import pandas as pd
+import numpy as np
+import csv as csv
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+import pylab as pl
+from wordcloud import WordCloud
+from scipy.stats.stats import pearsonr
+from nltk.tokenize import RegexpTokenizer
+from stop_words import get_stop_words
+from nltk.stem.porter import PorterStemmer
+from gensim import corpora, models
+import gensim
+#%matplotlib inline
+
+
+pl.ion()
+pl.clf()
+
+# Consumer_Complaints.csv from http://catalog.data.gov/dataset/consumer-complaint-database
+df = pd.read_csv('Consumer_Complaints.csv', header=0)
+
+# Data overview: contents and size
+print df.dtypes
+print len(df.index)
+
+# Data cleaning
+df['Date received'] = pd.to_datetime(df['Date received'])
+df['Response'] = df['Timely response?'].map({'No' : 0, 'Yes' : 1}).astype(int)
+df=df.fillna({'Consumer disputed?' : 'No'})
+df['Disputed'] = df['Consumer disputed?'].map({'No' : 0, 'Yes' : 1}).astype(int)
+df = df[['Date received','Issue','Product','Response','Disputed']]
+
+
+# Compute Pearson's Correlation between Response and Disputed rate.
+corr   = df[['Response','Disputed']]
+test_x = corr['Response'].values
+test_y = corr['Disputed'].values
+print "pearson correlation"
+print pearsonr(test_x, test_y)
+
+
+####################
+# Complained Product
+df_sub = df[['Product']]
+df_sub['count'] = 1
+df_sub_grp = df_sub.groupby(['Product']).sum().sort_index(by='count', ascending=False).ix[0:5]
+print '\n Top 5 most frequently complained products'
+print df_sub_grp
+
+df_sub = df[df['Response'] == 0]
+df_sub = df_sub[['Product']]
+df_sub['count'] = 1
+df_sub_grp = df_sub.groupby(['Product']).sum().sort_index(by='count', ascending=False).ix[0:5]
+print '\n Top 5 most frequently complained products which are not Timely responded'
+print df_sub_grp
+
+df_sub = df[df['Disputed'] == 1]
+df_sub = df_sub[['Product']]
+df_sub['count'] = 1
+df_sub_grp = df_sub.groupby(['Product']).sum().sort_index(by='count', ascending=False).ix[0:5]
+print '\n Top 5 most frequently complained products which are Consumer disputed'
+print df_sub_grp
+
+# Annual list viz
+df_sub = df[['Product']]
+df_sub['count'] = 1
+
+Product_List=[]
+for i in range(0,3):
+    Product_List.append(df_sub.groupby(['Product']).sum().sort_index(by='count', ascending=False).ix[i].name)
+
+
+date_list=[]
+count_0  =[]
+count_1  =[]
+count_2  =[]
+for i in range(0,len(df.index)):
+    date_list.append(df.ix[i]['Date received'])
+    if Product_List[0] == df.ix[i]['Product']:
+        count_0.append(1)
+    else:
+        count_0.append(0)
+    if Product_List[1] == df.ix[i]['Product']:
+        count_1.append(1)
+    else:
+        count_1.append(0)
+    if Product_List[2] == df.ix[i]['Product']:
+        count_2.append(1)
+    else:
+        count_2.append(0)
+
+tdf = pd.DataFrame({ Product_List[0] : count_0,
+                     Product_List[1] : count_1,
+                     Product_List[2] : count_2,},
+                   index=date_list)
+
+tdf_grp = tdf.groupby(level=0).sum().resample('A', how='sum')
+xticks = np.arange(2011,2016)
+print tdf_grp
+tdf_grp.plot(kind='bar', stacked=True, title="Top 3 most complained Products", rot=10)
+
+fig = plt.gcf()
+fig.savefig('bar.png')
+pl.clf()
+
+
+###################
+# Complained Issues
+df_sub = df[['Issue']]
+df_sub['count'] = 1
+df_sub_grp = df_sub.groupby(['Issue']).sum().sort_index(by='count', ascending=False).ix[0:10]
+print '\n Top 10 most frequently complained issues'
+print df_sub_grp
+
+df_sub = df[df['Response'] == 0]
+df_sub = df_sub[['Issue']]
+df_sub['count'] = 1
+df_sub_grp = df_sub.groupby(['Issue']).sum().sort_index(by='count', ascending=False).ix[0:10]
+print '\n Top 10 most frequent complained issues which are not Timely responded'
+print df_sub_grp
+
+df_sub = df[df['Disputed'] == 1]
+df_sub = df_sub[['Issue']]
+df_sub['count'] = 1
+df_sub_grp = df_sub.groupby(['Issue']).sum().sort_index(by='count', ascending=False).ix[0:10]
+print '\n Top 10 most frequent complained issues which are Consumer disputed'
+print df_sub_grp
+
+# More analysis with top issues
+df_sub = df[['Issue']]
+df_sub['count'] = 1
+
+Issue_List=[]
+for i in range(0,25):
+    Issue_List.append(df_sub.groupby(['Issue']).sum().sort_index(by='count', ascending=False).ix[i].name)
+
+# time series viz 
+date_list=[]
+count_all=[]
+count_0  =[]
+count_1  =[]
+for i in range(0,len(df.index)):
+    date_list.append(df.ix[i]['Date received'])
+    count_all.append(1)
+    if Issue_List[0] == df.ix[i]['Issue']:
+        count_0.append(1)
+    else:
+        count_0.append(0)
+    if Issue_List[1] == df.ix[i]['Issue']:
+        count_1.append(1)
+    else:
+        count_1.append(0)
+
+ts = pd.Series(count_all, index=date_list)
+ts_grp = ts.groupby(level=0).sum().resample('M', how='sum')
+
+ts0 = pd.Series(count_0, index=date_list)
+ts0_grp = ts0.groupby(level=0).sum().resample('M', how='sum')
+
+ts1 = pd.Series(count_1, index=date_list)
+ts1_grp = ts1.groupby(level=0).sum().resample('M', how='sum')
+
+plt.plot(ts_grp.index,ts_grp,label='total')
+plt.plot(ts0_grp.index,ts0_grp,label='1st complaint')
+plt.plot(ts1_grp.index,ts1_grp,label='2st complaint')
+
+plt.legend(loc=0)
+plt.ylabel('Number of Complaints in a month')
+plt.xlabel('Date')
+plt.savefig('ComplainCount')
+pl.clf()
+
+# Text analysis for top issuess with LDA
+# Initialize text 
+tokenizer = RegexpTokenizer(r'[A-Za-z0-9\']+')    # set tokenize Reg 
+en_stop = get_stop_words('en')         # create English stop words list
+p_stemmer = PorterStemmer()            # Create p_stemmer of class PorterStemmer
+texts = []                             # list for tokenized documents in loop
+text_view = ''
+
+# loop through document list
+for i in Issue_List:
+    # clean and tokenize document string
+    raw = i.lower()
+    tokens = tokenizer.tokenize(raw)
+
+    # remove stop words from tokens
+    stopped_tokens = [i for i in tokens if not i in en_stop]
+    
+    # stem tokens and add them to list
+    stemmed_tokens = [p_stemmer.stem(i) for i in stopped_tokens]
+    texts.append(stemmed_tokens)
+
+    #print ' '.join(stemmed_tokens)
+    text_view += ' '.join(stemmed_tokens)
+    text_view += ' '
+
+# turn our tokenized documents into a id <-> term dictionary
+dictionary = corpora.Dictionary(texts)
+    
+# convert tokenized documents into a document-term matrix
+corpus = [dictionary.doc2bow(text) for text in texts]
+
+# generate LDA model
+ldamodel = gensim.models.ldamodel.LdaModel(corpus, num_topics=5, id2word = dictionary, passes=20)
+print "\n Topic analysis result for top 25 issues with LDA"
+print(ldamodel.print_topics(num_topics=5, num_words=3))
+
+wordcloud = WordCloud(max_font_size=40).generate(text_view)
+plt.figure()
+plt.title("Top issue words")
+plt.imshow(wordcloud)
+plt.axis("off")
+plt.savefig('ComplainCount_WC')
--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # ConsumerComplaintsDataProject
+This is my data science side project.
+The data Consumer_Complaints.csv comes from http://catalog.data.gov/dataset/consumer-complaint-database.
+I wrote a python analysis script, ConsumerComplaints_Analy.py.
+This script performs following analyses as follow.
+[Outputs are in output.dat file and there are three plots, bar.png, ComplainCount.png, and ComplainCount_WC.png.]
+
+1) It demonstrates the contents and size of the file.
+   Main contents, which I use in this analysis are 'Date received','Issue','Product','Timely response?', and 'Consumer disputed?'
+
+2) Compute the Pearson's correlation between 'Timely response?' and 'Consumer disputed?'.
+   I find that there is very little correlation, correlation coefficient = 0.0334 and p-value ~ 0.
+
+3) I select "Top 5 most frequently complained products", "Top 5 most frequently complained products which are not Timely responded", and "Top 5 most frequently complained products which are Consumer disputed" and I find "Mortgage" and "Debt collection" are top 2 products.
+   "Debt collection" complaint gets less timely response while "Mortgage" complaint gets the highest rate to Consumer disputed.
+   I also plot the time evolution of the top 3 complained products year between 2011 to 2015 in bars.png.
+
+4) I select "Top 5 most frequently complained issues", "Top 5 most frequently complained issues which are not Timely responded", and "Top 5 most frequently complained issues which are Consumer disputed" and I find "Loan modification,collection,foreclosure".
+   "Cont'd attempts collect debt not owed" complaint, which is 4th most frequently complained issue, gets less timely response.
+   "Loan modification,collection,foreclosure" complaint, which is 1st most frequently complained issue, gets the highest rate to Consumer disputed.
+   In addition, I plot the time evolution of the total number of complaint with 1st and 2nd most frequently complained issues in ComplainCount.png.
+
+5) Lastly, I perform a topic modeling with top 25 most frequently complained issues using LDA method.
+   The output of the LDA analysis are [u'0.076*loan + 0.041*servic + 0.041*payment', u'0.035*money + 0.035*make + 0.035*send', u'0.082*credit + 0.056*report + 0.056*collect', u'0.071*account + 0.071*close + 0.039*problem', u'0.050*ident + 0.050*theft + 0.050*fraud']
+   The realted result is also shown in ComplainCount_WC.png as word cloud.
